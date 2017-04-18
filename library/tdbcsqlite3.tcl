3292ff5bc7fc09442ac8c06af8e2bbd51c062025
dgp <dgp>
2017-04-18 17:04:19 +0000

    Bump to 1.0.5 (TEA changes only).

--- library/tdbcsqlite3.tcl
+++ library/tdbcsqlite3.tcl
@@ -13,7 +13,7 @@
 package require tdbc
 package require sqlite3
 
-package provide tdbc::sqlite3 1.0.4
+package provide tdbc::sqlite3 1.0.5
 
 namespace eval tdbc::sqlite3 {
     namespace export connection
