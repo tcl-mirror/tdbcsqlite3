77822c20855e5d4ab2ffd9f0c5a6dc30cd3f7a09
jan.nijtmans <jan.nijtmans>
2020-02-12 12:50:35 +0000

    Version 1.1.1 -> 1.1.2. Update TEA

--- library/tdbcsqlite3.tcl
+++ library/tdbcsqlite3.tcl
@@ -13,7 +13,7 @@
 package require tdbc
 package require sqlite3
 
-package provide tdbc::sqlite3 1.1.1
+package provide tdbc::sqlite3 1.1.2
 
 namespace eval tdbc::sqlite3 {
     namespace export connection
